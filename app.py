b34013904f6e1656ca0cbfdcda430cfa13359ffb
Christopher Willis <willisc9@msu.edu>
2019-03-29 19:59:41 -0600

    added homepage

--- app.py
+++ app.py
@@ -1,44 +1,41 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 import random
+import pandas as pd
+import requests
+import quandl
 
 from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid, Range1d)
-#from bokeh.models.glyphs import VBar
 from bokeh.plotting import figure, output_file, show
-#from bokeh.charts import Bar
 from bokeh.embed import components
 from bokeh.models import ColumnDataSource, HoverTool
-
-#from bokeh.io import output_notebook
-#output_notebook()
+#from bokeh.models import TextInput, Button
 
 # connect the app
 app = Flask(__name__)
 
+# the dataframe function
+def GetData(ticker_symbol, columns):
+
+    quandl.ApiConfig.api_key = 'Y-vgM5syW3N9KmRk7nZY'
+
+    data = quandl.get_table('WIKI/PRICES', 
+        #qopts = { 'columns': ['ticker', 'date', 'open', 'close', 'adj_open', 'adj_close'] }, 
+        qopts = { 'columns': columns},
+        ticker = [ticker_symbol], 
+        date = { 'gte': '2016-01-01', 'lte': '2016-12-31' }, paginate=True)
+
+    return data
+
 # the plotting function
 def MakePlot(data):
 
+    # set the Bokeh column data source
     source = ColumnDataSource(data)
     print(source.column_names)
 
-    #source = ColumnDataSource(dict(
-    #    x = data.weight_kgs, 
-    #    y = data.height_m,
-    #)
-
-    #PLOT_OPTS = dict(
-    #    height=400, 
-    #    x_axis_type='log', 
-    #    y_axis_type='log', 
-    #    y_range=(0.08,20), 
-    #    x_range=(0.08,2000)
-    #)
-
-    hover = HoverTool(tooltips = [
-        ('days','@days'),
-        ('bugs','@bugs'),
-        ('costs','@costs')], 
-        show_arrow=False)
+    # set the tools
+    TOOLS = "pan, wheel_zoom, box_zoom, reset, save"
 
     #p = figure(title='Pokémon',
     #    x_axis_label='Weight [kg]',
@@ -47,79 +44,78 @@ def MakePlot(data):
     #    toolbar_location='right',
     #    tools=[hover], **PLOT_OPTS)
 
-    # create a new plot with a title and axis labels
-    p = figure(plot_height=250, title="Bug Counts",
-           toolbar_location=None, tools=[hover])
+    # initialize the canvas for plotting
+    p1 = figure(x_axis_type="datetime", title="Stock Prices 2016", tools=[TOOLS])
+    p1.grid.grid_line_alpha = 0.3
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Price'
     #p = figure(tools=[hover], **PLOT_OPTS)
 
-    p.vbar(x='days', top='bugs', width=0.9, source=source)
-    #p.circle(x='x', y='y', source=source, size=10, alpha=0.8, line_color='black', fill_color='pink')
+    ticker = str(data['ticker'].unique())
+    print(ticker)
 
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
-
-    #hover = create_hover_tool()
-    #plot = create_bar_chart(data, "Bugs per day", "days", "bugs", hover)
+    if('open' in data.columns): 
+    	p1.line(data['date'], data['open'], color='#A6CEE3', legend=ticker+': open')
+    if('close' in data.columns): 
+    	p1.line(data['date'], data['close'], color='#B2DF8A', legend=ticker+': close')
+    if('adj_open' in data.columns): 
+    	p1.line(data['date'], data['adj_open'], color='#33A02C', legend=ticker+': adj_open')
+    if('adj_close' in data.columns): 
+    	p1.line(data['date'], data['adj_close'], color='#FB9A99', legend=ticker+': adj_close')
+    
+    p1.legend.location = 'top_left'
 
     # return the finalized plot
-    return p
+    return p1
+
 
 
 # the homepage
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def homepage():
 
-	# get the data, from somewhere...
-    # (here, dictionary mapping string to list)
-    bars_count = 11
-    data = {'days': [], 'bugs': [], 'costs': []}
-    for i in range(1, bars_count + 1):
-        data['days'].append(i)
-        data['bugs'].append(random.randint(1,100))
-        data['costs'].append(random.uniform(1.00, 1000.00))
+    ticker_symbol = None
+    close_price = None
+    open_price = None
+    adj_close_price = None
+    adj_open_price = None
 
-    # setup the plot
-    p = MakePlot(data)
-    script, div = components(p)
+    # this block is only executed once the form is submitted
+    if request.method == 'POST':
 
-    print(script)
-    print(div)
+        # get the submitted form values...
+        ticker_symbol = request.form.get('ticker')
+        close_price = request.form.get('close')
+        open_price = request.form.get('open')
+        adj_close_price = request.form.get('adj_close')
+        adj_open_price = request.form.get('adj_open')
 
-    # render the html page
-    return render_template("chart.html", the_div=div, the_script=script)
+        # .. and put them a list, removing any 'None' entries
+        columns = ['ticker', 'date', open_price, close_price, adj_close_price, adj_open_price]
+        columns = [col for col in columns if col is not None]
+
+        # build the dataframe using the submitted values
+        data = GetData(ticker_symbol, columns)
+        print(data)
+
+        # make the plot and extract its components for html rendering
+        p = MakePlot(data)
+        script, div = components(p)
+        #print(script)
+        #print(div)
+
+        # render the html page with the plot embedded
+        return render_template("chart.html", the_div=div, the_script=script)
+
+    else:
+
+        return render_template('login.html', ticker_symbol=ticker_symbol)
 
 
 
 
 
 
-
-
-
-
-
-#@app.route('/')
-#def index():
-#    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % subpath
 
 
 
@@ -128,9 +124,4 @@ def show_subpath(subpath):
 
 if __name__ == '__main__':
  	app.run(port=33507)
-
-
-
-
-
 
